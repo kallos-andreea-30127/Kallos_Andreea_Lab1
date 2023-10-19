9a642a186cce912d37152c19208093eb4d4ae3c2
Andrei Mirt <andii_mihaela@yahoo.com>
2023-10-19 18:20:18 +0300

    Add controller

---
+++ Controllers/WelcomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kallos_Andreea_Lab1.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        public string Index()
+        {
+            return "Welcome!";
+        }
+
+        public string sayHello()
+        {
+            return "Hello from Andreea!";
+        }
+
+        public string showData(string msg, int nbr)
+        {
+            return "Your message is " + msg + " and your value is " + nbr;
+        }
+    }
+}
